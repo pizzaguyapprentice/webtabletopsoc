bcfe42e05c94804b94138e1f71c6505fd98501c1
pizzaguyapprentice <matisew8247@gmail.com>
2025-05-27 03:53:59 +0100

    Initial commit

---
+++ app/public/index.php
@@ -0,0 +1,5 @@
+
+<?php
+    include_once('./onload/header.php');
+    include_once('./onload/navbar.php');
+?>
---
+++ app/public/onload/navbar.php
@@ -0,0 +1,34 @@
+<link rel="stylesheet" href="onload/navbar.css">
+
+
+<div class="navbar">
+    <div class="menu">
+
+        <!-- Icon to index -->
+        <div class="menu-item">
+            <img class="menu-item-icon" src="/img/icon.png">
+        </div>
+        
+        <!-- Searchbar -->
+        <div class="menu-item">
+            <img src="img/searchicon.svg">
+        </div>
+
+        <!-- Join us button -->
+        <div class="menu-item">
+            
+        </div>
+
+        <!-- Discord button -->
+        <div class="menu-item">
+
+        </div>
+
+        <!-- Instagram button -->
+        <div class="menu-item">
+
+        </div>
+
+    </div>
+
+</div>
